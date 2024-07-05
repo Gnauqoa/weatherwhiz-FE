904f24e29e792bb0c7d0ddeb28fd3b2cbf82a92f
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-05 16:28:34 +0700

    Clean type.

--- src/@types/artist.ts
+++
@@ -1,9 +0,0 @@
-export type Artist = {
-  id: number;
-  name: string;
-  birth: string | null;
-  description: string | null;
-  created_at: string;
-  avatar_url: string;
-  updated_at: string;
-};
