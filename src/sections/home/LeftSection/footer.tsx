fa8684b5bc1be239b7e987a74c45139da42e4d79
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-06 01:29:30 +0700

    Fix left section footer.

--- src/sections/home/LeftSection/footer.tsx
+++ src/sections/home/LeftSection/footer.tsx
@@ -8,8 +8,9 @@ const Footer = () => {
   return (
     <div className="flex flex-row items-center justify-center gap-4 w-full">
       <DayCard current={true} {...data.current} id="current" />
-      {data.forecast.forecastday.map((forecastday, index) => (
+      {data.forecast.forecastday.slice(1).map((forecastday, index) => (
         <DayCard
+          key={`daycard-${index}`}
           current={false}
           {...forecastday.hour[0]}
           id={`daycard-${index}`}
