ed4469fef4ab5cf889649cc44185dd313f554b09
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-05 11:35:12 +0700

    Add login to useAuth hook.

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useDispatch, useSelector } from "../redux/store";
 import { removeToken } from "../utils/local-storage";
 import { setUser } from "../redux/slices/user";
+import { SignInFormProps, signIn } from "../apis/auth";
+import { toast } from "react-toastify";
 
 const useAuth = () => {
   const { isLoading, user } = useSelector((state) => state.user);
@@ -11,9 +13,22 @@ const useAuth = () => {
     dispatch(setUser(null));
     axios.defaults.headers.common["Authorization"] = null;
   };
-  if (isLoading || !user) return { isAuth: false, user, isLoading };
+  const login = async (data: SignInFormProps) => {
+    try {
+      removeToken();
+      axios.defaults.headers.common["Authorization"] = "";
+      await signIn(data);
+      toast("Login success", { type: "success" });
+    } catch (err) {
+      toast((err as any).response?.data?.error?.errors.join("."), {
+        type: "error",
+      });
+    }
+  };
+  if (isLoading || !user)
+    return { logout, login, isAuth: false, user, isLoading };
 
-  return { logout, isAuth: true, user, isLoading };
+  return { logout, login, isAuth: true, user, isLoading };
 };
 
 export default useAuth;
